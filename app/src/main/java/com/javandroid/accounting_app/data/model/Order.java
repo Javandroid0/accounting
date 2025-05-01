9a9236ba6e7758a18815bf89056a09196639d927
javandroid <morteza015@gmail.com>
2025-05-01 21:23:21 +0330

    scanner done

--- app/src/main/java/com/javandroid/accounting_app/data/model/Order.java
+++ app/src/main/java/com/javandroid/accounting_app/data/model/Order.java
@@ -17,9 +17,11 @@ public class Order {
     private double productBuyPrice;
     private double quantity;
 
-    public Order(){}
+    public Order() {
 
-    public Order(String currentUserId, int id, String name,String productBarcode, int i, double sellPrice,double productBuyPrice) {
+    }
+
+    public Order(String currentUserId, int id, String name, String productBarcode, int i, double sellPrice, double productBuyPrice) {
         this.userId = currentUserId;
         this.productId = id;
         this.productName = name;
@@ -31,26 +33,61 @@ public class Order {
 
 
     // Getters & Setters
-    public int getOrderId() { return orderId; }
-    public void setOrderId(int orderId) { this.orderId = orderId; }
+    public int getOrderId() {
+        return orderId;
+    }
 
-    public String getUserId() { return userId; }
-    public void setUserId(String userId) { this.userId = userId; }
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
 
-    public int getProductId() { return productId; }
-    public void setProductId(int productId) { this.productId = productId; }
+    public String getUserId() {
+        return userId;
+    }
 
-    public String getProductName() { return productName; }
-    public void setProductName(String productName) { this.productName = productName; }
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
 
-    public double getProductSellPrice() { return productSellPrice; }
-    public void setProductSellPrice(double productSellPrice) { this.productSellPrice = productSellPrice; }
+    public int getProductId() {
+        return productId;
+    }
 
-    public double getProductBuyPrice() { return productBuyPrice; }
-    public void setProductBuyPrice(double productBuyPrice) { this.productBuyPrice = productBuyPrice; }
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
 
-    public double getQuantity() { return quantity; }
-    public void setQuantity(double quantity) { this.quantity = quantity; }
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public double getProductSellPrice() {
+        return productSellPrice;
+    }
+
+    public void setProductSellPrice(double productSellPrice) {
+        this.productSellPrice = productSellPrice;
+    }
+
+    public double getProductBuyPrice() {
+        return productBuyPrice;
+    }
+
+    public void setProductBuyPrice(double productBuyPrice) {
+        this.productBuyPrice = productBuyPrice;
+    }
+
+    public double getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(double quantity) {
+        this.quantity = quantity;
+    }
 
     public String getProductBarcode() {
         return productBarcode;
